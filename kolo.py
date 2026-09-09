03fcb7435634b1a770f07216e58ed87097d47d1d
knowak <140261646+krystianowak@users.noreply.github.com>
2026-09-09 18:39:11 +0200

    Change back to float instead of (float or int)

--- kolo.py
+++ kolo.py
@@ -1,5 +1,5 @@
 import math
 
-r = (float or int)(input("Podaj promień koła: "))
+r = float(input("Podaj promień koła: "))
 
 print(f"Pole powierzchni koła o promieniu {r} jest równe {math.pi*(r*r)}")
