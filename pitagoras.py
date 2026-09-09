e236c573cf595d55baa9780eac2fdae1e19b1ac8
knowak <140261646+krystianowak@users.noreply.github.com>
2026-09-09 18:40:28 +0200

    Go back to "float" and stop using "(float or int)"

--- pitagoras.py
+++ pitagoras.py
@@ -1,6 +1,6 @@
 from math import sqrt
 
-a = (float or int)(input("Podaj bok numer 1: "))
-b = (float or int)(input("Podaj bok numer 2: "))
+a = float(input("Podaj bok numer 1: "))
+b = float(input("Podaj bok numer 2: "))
 
 print(f"Bok trzeci, kiedy bok pierwszy jest równy {a} a bok drugi jest równy {b} jest równy {sqrt((a*a)+(b*b))}")
