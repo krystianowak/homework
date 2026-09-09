1673be4006698cd4ca133963614b94cb041f0d84
knowak <140261646+krystianowak@users.noreply.github.com>
2026-09-09 18:56:31 +0200

    Go back to "float" and stop using "(float or int)"

--- rownoleglobok.py
+++ rownoleglobok.py
@@ -1,4 +1,4 @@
-a = (float or int)(input("Podaj długość podstawy: "))
-h = (float or int)(input("Podaj wysokość: "))
+a = float(input("Podaj długość podstawy: "))
+h = float(input("Podaj wysokość: "))
 
 print(f"Pole równoległoboku o podstawie {a} i wysokości równej {h} jest równe {a*h}")
