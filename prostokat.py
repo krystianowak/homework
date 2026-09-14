77886d22f7aeab7ea182cdeb2549d11adf8a0223
knowak <140261646+krystianowak@users.noreply.github.com>
2026-09-14 20:50:00 +0200

    Delete prostokat.py

--- prostokat.py
+++
@@ -1,7 +0,0 @@
-a = float(input("Wpisz długość boku numer 1: "))
-b = float(input("Wpisz długość boku numer 2: "))
-
-if b == a:
-    print("Długość boków prostokąta nie może być taka sama")
-    exit()
-print(f"Pole powierzchni prostokąta o boku numer 1 równym {a} i boku numer 2 równym {b} jest równe {a*b}")
